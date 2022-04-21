0611d64c6ce464aee71b732567ab40b8cd90f070
Cristiane Rodrigues da Silva <73915079+crodsil@users.noreply.github.com>
2022-04-21 18:59:48 -0300

    Add files via upload
    Trabalho final Frontend

--- VERSAOFINAL/CheckPointII-main/scripts/tarefas/tarefas.js
+++ VERSAOFINAL/CheckPointII-main/scripts/tarefas/tarefas.js
@@ -1,4 +1,53 @@
-let novaTarefa = document.getElementById("novaTarea");
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.getElementById("ul");
+var item = document.getElementsByTagName("li");
+
+function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
+    var li = document.createElement("li");
+
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+
+    function crossOut(){
+        li.classList.toggle("done");
+    }
+
+    li.addEventListener("click",crossOut);
+
+    var dBtn = document.createElement("button");
+    dBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(dBtn);
+    dBtn.addEventListener("click", deleteListItem);
+
+    function deleteListItem(){
+        li.classList.add("delete");
+    }
+}
+
+enterButton.addEventListener("click",addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+function addListAfterClick(){
+    if (inputLength() > 0){
+        createListElement();
+    }
+}
+    
+function addListAfterKeypress(){
+    if(inputLength() > 0 && event.which === 13){
+        createListElement();
+    }
+}
+
+
+
+/*let novaTarefa = document.getElementById("novaTarea");
 let botaoNovaTarefa = document.getElementById("botaoNovaTarefa")
 
 let criandoTarefas = {
@@ -13,11 +62,7 @@ botaoNovaTarefa.addEventListener('click', function(event){
 
     event.preventDefault();
 
-
-
-
-
-})
+})*/
 
 
 
